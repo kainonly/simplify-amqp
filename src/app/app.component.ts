ce4d2cfd38046768fe81a21643f26b5adaac5456
kain <kainpro@foxmail.com>
2018-11-24 08:45:32 +0800

    初始化前端

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,10 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  dataSet = [
-    {
-      type: '字符串',
-      key: 'admin_token',
-    },
-    {
-      type: '哈希',
-      key: 'admin_lists',
-    },
-    {
-      type: '集合',
-      key: 'express_order',
-    },
-    {
-      type: '组',
-      key: 'admin'
-    },
-  ];
-
-  editorOptions = {theme: 'vs-dark', language: 'javascript'};
-  code = 'function x() {\nconsole.log("Hello world!");\n}';
-
+  title = 'zredis-manager';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,16 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
-import {NgZorroAntdModule} from 'ng-zorro-antd';
-import {PerfectScrollbarModule} from 'ngx-perfect-scrollbar';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import {AppComponent} from './app.component';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    NgZorroAntdModule,
-    PerfectScrollbarModule,
+    BrowserModule
   ],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule { }
