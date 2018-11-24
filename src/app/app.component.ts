0a5547cb239c45aff1c0e65870c406cf838c64c6
kain <kainpro@foxmail.com>
2018-11-24 11:16:05 +0800

    提交基本ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NzTreeNodeOptions} from 'ng-zorro-antd';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'zredis-manager';
+export class AppComponent implements OnInit {
+  nodes: NzTreeNodeOptions[] = [{
+    title: '腾讯云',
+    key: '1',
+    type: 'project',
+    children: [{
+      title: 'db0',
+      key: 'db0',
+      type: 'database',
+      children: [{
+        title: 'admin-hash',
+        key: '11',
+        isLeaf: true
+      }, {
+        title: 'role-hash',
+        key: '12',
+        isLeaf: true
+      }]
+    }, {
+      title: 'db1',
+      key: 'db1',
+      type: 'database',
+    }]
+  }, {
+    title: '阿里云',
+    key: '2',
+    type: 'project',
+  }];
+
+  ngOnInit() {
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {registerLocaleData} from '@angular/common';
+import {HttpClientModule} from '@angular/common/http';
+import {NgZorroAntdModule, NZ_I18N, zh_CN} from 'ng-zorro-antd';
+import zh from '@angular/common/locales/zh';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+
+registerLocaleData(zh);
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    NgZorroAntdModule
   ],
-  providers: [],
+  providers: [{provide: NZ_I18N, useValue: zh_CN}],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
