64c8c19d047b67d1fa9b39aba1a4a3f7736f7ff3
kain <kainpro@foxmail.com>
2019-03-27 21:20:36 +0800

    submit

---
+++ authenticate.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"github.com/dgrijalva/jwt-go"
+	"github.com/segmentio/ksuid"
+	"time"
+)
+
+type (
+	Symbol map[string]interface{}
+)
+
+func main() {
+
+}
+
+func SetToken() {
+	var hmacSampleSecret = []byte("e8NhjiHq86IVfy3nLqN9IdriYryyBPX4K7gNwAaU")
+	jti := ksuid.New().String()
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"jti":    jti,
+		"aud":    "",
+		"iss":    "",
+		"exp":    time.Now().Unix() + 1800,
+		"user":   "",
+		"role":   "",
+		"symbol": Symbol{},
+	})
+
+	tokenString, err := token.SignedString(hmacSampleSecret)
+	if err != nil {
+		println(err.Error())
+	}
+	println(tokenString)
+}
+
+func CheckToken() {
+
+}
