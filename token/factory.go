0a24782018f45a65e825cc2ef5efb8a75f6aa510
kain <kainpro@foxmail.com>
2019-04-19 09:58:18 +0800

    Submit

--- token/factory.go
+++ token/factory.go
@@ -5,12 +5,23 @@ import (
 	"time"
 )
 
+type CustomClaims struct {
+	jwt.StandardClaims
+}
+
 func Factory() string {
 	hmacSampleSecret := []byte("WgjCA%1n8ZidI1Qt")
 
-	token := jwt.NewWithClaims(jwt.SigningMethodHS384, jwt.MapClaims{
-		"foo": "bar",
-		"nbf": time.Now().Unix(),
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, CustomClaims{
+		jwt.StandardClaims{
+			Audience:  "somebody",
+			ExpiresAt: time.Now().Unix(),
+			Id:        "123",
+			IssuedAt:  time.Now().Unix(),
+			Issuer:    "kain",
+			NotBefore: time.Now().Unix(),
+			Subject:   "asd",
+		},
 	})
 
 	tokenString, _ := token.SignedString(hmacSampleSecret)
